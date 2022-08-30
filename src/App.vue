a3d50ac43b0cc951ac7acc12b18f4b047679b412
PollyPO Lee <polly0215yu@gmail.com>
2022-08-30 12:43:47 +0800

    testing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  {{ message }}
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup () {
+    let message = ref('Hello')
+
+    onMounted(() => {
+      message = 'Hi'
+    })
+
+    return {
+      message
+    }
   }
 }
 </script>
